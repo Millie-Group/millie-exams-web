de0fc2cd7a03bcd0f8dfebe9f21998a830103565
judehunter <matisowagm@gmail.com>
2020-12-10 21:51:56 +0100

    change nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,8 +56,6 @@ module.exports = {
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
-    {src: '@/plugins/infscroll.js', ssr: false},
-    {src: '@/plugins/portal.js', ssr: true},
     {src: '@/plugins/vuexpersist.js', ssr: false}
   ],
   /*
@@ -79,132 +77,12 @@ module.exports = {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/style-resources',
-    '@nuxtjs/auth-next',
-    ['@nuxtjs/robots', process.env.STAGE === 'production' ? {
-      UserAgent: '*',
-      Allow: '/'
-    } : {
-      UserAgent: '*',
-      Disallow: '/'
-    }],
-    '@nuxtjs/gtm',
-    '@nuxtjs/sitemap',
-    '@nuxtjs/sentry'
   ],
-
-  sentry: {
-    dsn: process.env.SENTRY_DSN,
-    disabled: process.env.NODE_ENV === 'development',
-    config: {
-      environment: process.env.STAGE || 'development'
-    }
-  },
-
-  sitemap: {
-    hostname: 'https://ensembleeducation.org',
-    changefreq: 'daily',
-    exclude: [
-      '/admin',
-      '/admin/**',
-      '/logout',
-      '/welcome',
-      '/callback'
-    ],
-    routes: [
-      {
-        url: '/submit',
-        priority: 0.5,
-        lastmod: new Date()
-      },
-      {
-        url: '/login',
-        priority: 0.7,
-        lastmod: new Date()
-      },
-      {
-        url: '/signup',
-        priority: 0.8,
-        lastmod: new Date()
-      }
-    ],
-    defaults: {
-      priority: 1,
-      lastmod: new Date()
-    }
-  },
-
-  gtm: {
-    id: process.env.GTM_ID,
-    enabled: process.env.STAGE === 'production'
-  },
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {},
-
-  auth: {
-    cookie: {
-      options: {
-        maxAge: 60 * 60 * 24 * 30 * 12 * 3,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'Lax'
-      }
-    },
-    redirect: {
-      login: '/login',
-      callback: '/callback',
-      home: '/feed',
-      logout: '/login'
-    },
-    strategies: {
-      facebook: {
-        scheme: 'oauth2',
-        clientId: '1798890273583314',
-        endpoints: {
-          logout: false,
-          authorization: 'https://facebook.com/v6.0/dialog/oauth',
-          token: process.env.API_URL + 'auth/facebook',
-          userInfo: process.env.API_URL + 'me'
-        },
-        scope: ['public_profile', 'email'],
-        responseType: 'code',
-        token: {
-          maxAge: 60 * 60 * 24 * 30 * 12 * 3
-        }
-      },
-      google: {
-        scheme: 'oauth2',
-        clientId: '833386515779-oj8up1us0jfbmvpn9k6i2e79caof5440.apps.googleusercontent.com',
-        endpoints: {
-          logout: false,
-          authorization: 'https://accounts.google.com/o/oauth2/auth',
-          token: process.env.API_URL + 'auth/google',
-          userInfo: process.env.API_URL + 'me'
-        },
-        scope: ['email', 'profile', 'openid'],
-        responseType: 'code',
-        codeChallengeMethod: '',
-        token: {
-          maxAge: 60 * 60 * 24 * 30 * 12 * 3
-        }
-      },
-      local: {
-        token: {
-          property: 'access_token',
-          maxAge: 60 * 60 * 24 * 30 * 12 * 3
-        },
-        endpoints: {
-          login: {url: process.env.API_URL + 'auth/local/login', method: 'post'},
-          logout: false,
-          user: {url: process.env.API_URL + 'me', method: 'get'}
-        },
-        user: {
-          property: false
-        }
-      }
-    }
-  },
 
   styleResources: {
     scss: [
